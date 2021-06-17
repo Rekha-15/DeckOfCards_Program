eedb621c6d480e1a1cf6c20fcba01f5b769e9ec7
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-17 10:32:58 -0400

    add : Welcome statement : .

---
+++ DeckOfCards.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * Purpose : Implementing Deck Of Cards Game.
+ * @author Rekha
+ * @Version 1.0
+ * @Since 17-06-2021
+ *
+ *******************************************************************************/
+package com;
+
+public class DeckOfCards {
+
+	/**
+	 * Main method
+	 *Printing welcome statement
+	 */
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Deck Of Cards Program");
+		
+	}
+
+}
